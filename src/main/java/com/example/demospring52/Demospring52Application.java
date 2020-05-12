c9157361a2f6ae73992394276c540a8e9f6cbb8b
hyeop <totoll13mail@gmail.com>
2020-05-12 09:58:10 +0900

    IoC 컨테이너 7부: MessageSource

--- src/main/java/com/example/demospring52/Demospring52Application.java
+++ src/main/java/com/example/demospring52/Demospring52Application.java
@@ -2,7 +2,10 @@ package com.example.demospring52;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties")
@@ -10,6 +13,16 @@ public class Demospring52Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demospring52Application.class,args);
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        // messages.properties 수정(Build)할 시 실시간으로 바뀜.
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);               // 캐쉬 3초로 설정.
+        return messageSource;
     }
 
 }
