e616942546a2d7daf7698bf233ca31e2109d6d62
hyeop <totoll13mail@gmail.com>
2020-05-12 14:37:15 +0900

    IoC 컨테이너 9부: ResourceLoader Maven(pom.xml) JDK 11로 변경

--- src/main/java/com/example/demospring52/AppRunner.java
+++ src/main/java/com/example/demospring52/AppRunner.java
@@ -4,13 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher applicationEventPublisher;
+    ResourceLoader resourceLoader;
+
+//    @Autowired
+//    ApplicationEventPublisher applicationEventPublisher;
+
 //    @Autowired
 //    MessageSource messageSource;
 
@@ -31,8 +42,13 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        applicationEventPublisher.publishEvent(new MyEventOld(this,100));
-        applicationEventPublisher.publishEvent(new MyEvent(this,200));
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
+
+//        applicationEventPublisher.publishEvent(new MyEventOld(this,100));
+//        applicationEventPublisher.publishEvent(new MyEvent(this,200));
 //        while (true) {
 //            System.out.println(messageSource.getMessage("greeting",new String[]{"keesun"}, Locale.KOREA));
 //            System.out.println(messageSource.getMessage("greeting",new String[]{"keesun"}, Locale.US));
--- src/main/java/com/example/demospring52/MyEventHandler.java
+++ src/main/java/com/example/demospring52/MyEventHandler.java
@@ -17,21 +17,21 @@ public class MyEventHandler {
         System.out.println("메세지를 받았다. (4.2 이후) 데이터는 : " + event.getData());
     }
 
-    @EventListener
-    @Async
-    public void handle(ContextRefreshedEvent event){
-        System.out.println(Thread.currentThread().toString());
-        System.out.println("ContextRefreshedEvent");
-        // ApplicationContext를 초기화 했거나, 리프레쉬 했을 때 발생
-
-    }
-
-    @EventListener
-    @Async
-    public void handle(ContextClosedEvent event){
-        System.out.println(Thread.currentThread().toString());
-        System.out.println("ContextClosedEvent");
-        // ApplicationContext를 close()하여 싱글톤 빈이 소멸되는 시점에 발생.
-
-    }
+//    @EventListener
+//    @Async
+//    public void handle(ContextRefreshedEvent event){
+//        System.out.println(Thread.currentThread().toString());
+//        System.out.println("ContextRefreshedEvent");
+//        // ApplicationContext를 초기화 했거나, 리프레쉬 했을 때 발생
+//
+//    }
+//
+//    @EventListener
+//    @Async
+//    public void handle(ContextClosedEvent event){
+//        System.out.println(Thread.currentThread().toString());
+//        System.out.println("ContextClosedEvent");
+//        // ApplicationContext를 close()하여 싱글톤 빈이 소멸되는 시점에 발생.
+//
+//    }
 }
