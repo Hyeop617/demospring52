42c53463abfaf0bf143acb2333ef2ea7d5589626
hyeop <totoll13mail@gmail.com>
2020-05-13 13:47:22 +0900

    Resource 추상화

--- src/main/java/com/example/demospring52/AppRunner.java
+++ src/main/java/com/example/demospring52/AppRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -42,7 +43,20 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+//        var ctx = new GenericApplicationContext("xxx.xml");
+
+        System.out.println(resourceLoader.getClass());
         Resource resource = resourceLoader.getResource("classpath:test.txt");
+        // 스프링 부트 내장 톰캣에는 ContextPath가 지정되있지 않음. (ServletContextResource 의 경우)
+        // 따라서, ContextPath를 따로 지정하지 않을 시에는 에러가 뜸. 그래서 classpath를 추천.
+        System.out.println(resource.getClass());
+        /*
+         * location에 classpath안적으면 ServletcontextResource 기준으로 찾음. (현재 ApplicationContext가 WebServerApplicationContext이기 때문)
+         * ex) classpath:me/whiteship/config.xml        -> ClassPathResource
+         * ex) file:///some/resource/config.xml         -> FileSystemResource
+         * ex) 나머지는 ServletContextResource
+         * 이렇게 접두사를 붙이는 것을 추천!! (명시적이기 때문)
+         */
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
